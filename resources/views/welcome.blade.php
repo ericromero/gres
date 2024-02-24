db885cec5ec96da0f01be8afede35271c1cfc3d8
Eric Romero <eric.romero.mtz@gmail.com>
2024-02-23 23:05:50 -0600

    Vista Welcome actualizada con directivas de tailwindcss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,37 +22,26 @@
                 <div class="grid gap-4 grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
 
                     {{-- Inicia el ciclo para publicar todos los eventos --}}
+                    
+
                     @foreach ($events as $event)
-                        <div class="{{ $event->cancelled == 1 ? 'bg-red-50' : 'bg-white' }} dark:bg-gray-800 border border-gray-700 dark:border-gray-100 overflow-hidden rounded-lg shadow-sm sm:rounded-lg mb-4" style="min-width: 200px; max-width: 400px;min-height: 350px; y max-height: 500px;">
+                        <div class="{{ $event->cancelled == 1 ? 'bg-red-50' : 'bg-white' }} dark:bg-gray-800 border border-gray-700 dark:border-gray-100 overflow-hidden rounded-lg shadow-sm sm:rounded-lg mb-4">
                             @if($event->cancelled == 1)
                                 <div class="text-red-700 text-center font-bold">EVENTO CANCELADO</div>
                             @endif
-                            {{-- <h2 class="text-xl font-semibold p-4">{{ $event->title }}</h2> --}}
-                            <div class="relative" style="min-height: 300px; max-height: 500px;min-height: 350px; y max-height: 500px;">
-                                <a href="{{ route('events.show', ['event' => $event]) }}">
-                                    <div class="absolute inset-0 bg-cover bg-center hover:bg-opacity-80 transition duration-300" style="background-image: url('{{asset($event->cover_image)}}');">
-                                    </div>
-                                </a>
-                                <div class="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white p-4">
-                                    {{-- <p class="text-sm"><strong>Fecha:</strong> {{ $event->start_date }}
-                                        @if($event->start_date!=$event->end_date)
-                                        - {{ $event->end_date }}
-                                        @endif
-                                    </p>
-                                    <p class="text-sm"><strong>Horario:</strong> {{ $event->start_time }} - {{ $event->end_time }}</p> --}}
-                                    <p class="text-sm">{{ $event->date_time_text }}</p>
 
-                                    {{-- <p class="text-sm"><strong>Lugar:</strong>
-                                        @foreach($event->spaces as $event_space)
-                                            {{$event_space->name}} ({{$event_space->location}})<br>
-                                        @endforeach
-                                    </p> --}}
-                                    @if ($event->registration_url!=null)
-                                        <p class="text-sm"><strong>Registro:</strong> {{ $event->registration_url }}</p>
-                                    @else
-                                        <p class="text-sm">Entrada libre.</p>
-                                    @endif
+                            <div class="relative">
+                                <img src="{{asset($event->cover_image)}}" alt="{{ $event->title }}" class="w-full h-40 object-cover rounded-t-lg">
+
+                                <div class="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white p-2">
+                                    <p class="text-sm">{{ $event->date_time_text }}</p>
                                 </div>
+
+                                @if ($event->registration_url!=null)
+                                    <p class="text-sm"><strong>Registro:</strong> {{ $event->registration_url }}</p>
+                                @else
+                                    <p class="text-sm">Entrada libre.</p>
+                                @endif
                             </div>
                         </div>
                     @endforeach
